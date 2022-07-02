5e9b825b96babc9dc2850ded1ff23b83f2cbd36f
Br1Pire <104472439+Br1Pire@users.noreply.github.com>
2022-07-02 15:40:20 -0400

    cosa

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -1,15 +1,14 @@
 ﻿using Dominó;
 
-namespace Tester
+namespace ConsoleInterface
 {
    
 
-    class MyProgram
+    class Printer
     {
         static void Main(string[] args)
         {
-            Game x = new Game(new DoubleSixInToken(), new GetRandomFirstPlayer(), new RadomTokenDistributor(), new CommonEnd(), new List<IPlayer> { new HighScoreDropperPlayer(1), new HighScoreDropperPlayer(2), new HighScoreDropperPlayer(3), new HighScoreDropperPlayer(4) });
-            Console.WriteLine(x.Run());
+            
         }
     }
 }
